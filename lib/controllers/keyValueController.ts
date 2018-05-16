6efceace52f988c517b891a298a7aed6a59d48ef
Ayman <ayman4ze@gmail.com>
2018-05-16 23:59:49 +0800

    connect web api to mongodb

--- lib/controllers/keyValueController.ts
+++ lib/controllers/keyValueController.ts
@@ -8,11 +8,63 @@ const Value = mongoose.model('Value', ValueSchema);
 
 export class KeyValueController {
 
-    public async addNewKeyValue(req: Request, res: Response) {     
-        res.status(200).send("addNewKeyValue");  
+    public async addNewKeyValue(req: Request, res: Response) {
+
+        if (Object.keys(req.body).length == 0) {
+            res.status(400).send("{\"key\" : \"value\"} pair is required");
+            return;
+        }
+
+        let reqKey: string = Object.keys(req.body)[0];
+        let reqValue: string = req.body[reqKey];
+
+        try {
+            let dbKey = await Key.findOne({ "key": reqKey });
+            if (!dbKey) {
+                //key not exist.
+                dbKey = await new Key({ "key": reqKey }).save();
+            }
+            let newValue = await new Value({ keyId: dbKey._id, value: reqValue }).save();
+
+            res.status(201).send({ "key": dbKey.key, "value": newValue.value, "timestamp": newValue.created_date });
+        } catch (err) {
+            if (err.name == "ValidationError") {
+                res.status(400).send(err.message);
+            } else {
+                res.status(500).send(err);
+            }
+        }
     }
 
-    public async getValueByKey(req: Request, res: Response) {   
-        res.status(200).send("getValueByKey");      
+    public async getValueByKey(req: Request, res: Response) {
+
+        let timestamp;
+        let notFoundErrMsg: string;
+
+        if (req.query.timestamp) {
+            timestamp = req.query.timestamp;
+            notFoundErrMsg = "no value associated with key at this time";
+        } else {
+            timestamp = Math.floor(new Date().getTime() / 1000);
+            notFoundErrMsg = "no value associated with key";
+        }
+
+        try {
+            let dbKey = await Key.findOne({ "key": req.params.key });
+            if (!dbKey) {
+                //key not exist.
+                res.status(404).send("key not exist.");
+                return;
+            }
+
+            let dbValue = await Value.find({ keyId: dbKey._id, created_date: { $lte: timestamp } }).sort({ "created_date": -1 }).limit(1);
+            if (dbValue.length == 0) {
+                res.status(404).send(notFoundErrMsg);
+            } else {
+                res.status(200).send({ "value": dbValue[0].value });
+            }
+        } catch (err) {
+            res.status(500).send(err);
+        }
     }
 }
